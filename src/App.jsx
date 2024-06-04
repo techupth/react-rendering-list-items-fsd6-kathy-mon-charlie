e464be6d6ef09622b4d8d241f622a0c68117a809
Mon <tracer.coolkid@gmail.com>
2024-06-04 13:26:02 +0700

    feat:add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import MovieList from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
         {/* Render Movie Lists Here */}
+        <h1 className="movie-list-header">Movie List Section</h1>
+        <MovieList />
+       
       </section>
     </div>
   );
