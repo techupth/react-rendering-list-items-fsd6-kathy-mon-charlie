c4efcee338a8c197f207fb23618503937eab17e5
CharlieHii <charlie_chaper@hotmail.com>
2024-06-04 22:27:53 +0700

    perf(src/*,src/components/MovieListSection.jsx): add flex wrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import "./App.css";
 import { MovieItem } from "./components/MovieListSection";
 import movies from "./data/movies";
+import teams from "./data/teams";
 
 function App() {
   return (
-    <div className="App bg-[#E0EAFF] font-[kanit]">
+    <div className="App bg-[#E0EAFF] font-[kanit] mb-[20px]">
       <section className="movie-list-section flex flex-col items-center">
         {/* Render Movie Lists Here */}
-        <h1 className="text-black">Movie List Section</h1>
-        {movies.map((item, key) => (
-          <MovieItem item={item} key={key} />
-        ))}
+        <h1 className="text-[#000000] text-[40px] leading-[18px] mt-[69px] mb-[80px]">
+          Movie List Section
+        </h1>
+        <div className="flex justify-center items-center gap-[40px] flex-wrap">
+          {movies.map((item, key) => (
+            <MovieItem item={item} key={key} />
+          ))}
+        </div>
+      </section>
+      <section className="team-list-section flex flex-col items-center">
+        <h1 className="text-[#000000] text-[40px] leading-[18px] mt-[69px] mb-[80px]">
+          Team List Section
+        </h1>
       </section>
     </div>
   );
--- src/components/MovieListSection.jsx
+++ src/components/MovieListSection.jsx
@@ -1,27 +1,33 @@
 export function MovieItem(prop) {
   const movie = prop.item;
   return (
-    <div className="w-[400px] h-[250px] flex gap-[19px] bg-white mb-[80px] p-[20px] text-[#000000]">
+    <div className="w-[400px] h-[250px] flex text-[20px] rounded-[10px] gap-[19px] bg-[#FFFFFF] mb-[80px] p-[15px] pr-0 text-[#181818] shadow-[0_4px_40px_0_rgba(145,124,124,0.25)]">
       <img
         src={movie.image}
         alt={`${movie.title} image`}
-        className="w-[102px] h-[100px] rounded-[10px]"
+        className="w-[102px] h-[100px] rounded-[10px] object-cover"
       />
-      <div className="w-[231px] h-[211px]">
-        <p>Title: {movie.title}</p>
+      <div className="w-[231px] h-[211px] flex flex-col justify-between">
+        <div className="flex">
+          <p className="mr-[2px]">Title: </p>
+          <p>{movie.title}</p>
+        </div>
         <p>Year: {movie.year}</p>
         <p>Runtime: {movie.runtime}</p>
-        <p>
-          Genres:{" "}
-          {movie.genres.map((genresItem, genresIndex) => (
-            <span
-              key={genresIndex}
-              className="w-[68px] h-[27px] rounded-[10px] bg-[#EAAC99]  text-[16px] leading-[18px] mr-[9px]"
-            >
-              {genresItem}
-            </span>
-          ))}
-        </p>
+        <div className="flex">
+          <p>Genres: </p>
+          <div className="flex flex-wrap ml-[9px] gap-[3px]">
+            {movie.genres.map((genresItem, genresIndex) => (
+              <div
+                key={genresIndex}
+                className="h-[27px] px-[5px] py-[4.5px] rounded-[10px] bg-[#EAAC99] text-[16px] leading-[18px]"
+              >
+                <p>{genresItem}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
         <p>IMDB Ratings: {movie.imdbRating}</p>
         <p>IMDB Votes: {movie.imdbVotes}</p>
       </div>
